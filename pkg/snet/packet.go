d130188b5cf40d7711d5526ef071e9e7a1b4c93d
Juan A. Garcia Pardo <juagargi@gmail.com>
2024-02-29 14:10:48 +0100

    Add snet mock.

---
+++ pkg/snet/packet.go
@@ -0,0 +1,26 @@
+package snet
+
+import "fmt"
+
+// Global registry of packet handlers.
+var PacketHandlers map[string]PacketHandlerFunc
+
+// Handler function.
+type PacketHandlerFunc func(p *Packet) error
+
+func AddHandler(packetType string, handler PacketHandlerFunc) error {
+	if _, ok := PacketHandlers[packetType]; ok {
+		return fmt.Errorf("there already exists a handler for this type: %s", packetType)
+	}
+	PacketHandlers[packetType] = handler
+}
+
+// Packet is a packet.
+type Packet struct {
+	TypeOfPacket string
+	SizeOfPacket int
+}
+
+func (p *Packet) String() string {
+	return fmt.Sprintf("snet.Packet type: %s , size: %d", p.TypeOfPacket, p.SizeOfPacket)
+}
